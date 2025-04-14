456066d929f13a7b904d51fbb59d50c9a5ecbeca
Honore Amounan <78807934+HonoreBallar@users.noreply.github.com>
2025-04-14 20:10:25 +0000

    category screen

---
+++ src/components/CategoryCard.js
@@ -0,0 +1,19 @@
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+export default function CategoryCard(){
+    return(
+        <TouchableOpacity style={{height: 80, backgroundColor: '#f2f2f2', flexDirection: 'row', alignItems: 'center', borderRadius: 10}}>
+            <View style={{width: 60, height: 60, borderRadius: 30, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderWidth: 0.5, borderColor: '#ccc', marginLeft: 10}}>
+                <Image source={require('../assets/chaussure.jpg')} style={{width: 50, height: 50, borderRadius: 25}} resizeMode="contain"/>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '70%'}}>
+                <Text style={{marginLeft: 10, fontSize: 18, fontWeight: '500'}}>Women</Text>  
+                <View style={{flexDirection: 'row'}}>
+                    <Text style={{backgroundColor: '#A9C7FF', marginRight: 8, padding: 2, borderRadius: 5}}>10</Text>
+                    <FontAwesome5 name="chevron-right" size={15} color="black" />
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
--- src/screens/CategoryScreen.js
+++ src/screens/CategoryScreen.js
@@ -1,10 +1,26 @@
-import { ScrollView, Text } from "react-native"
+import { FlatList, ScrollView, Text, View } from "react-native"
 import Header from "../components/Header";
+import CategoryCard from "../components/CategoryCard";
+import { useCategories } from "../context/CategoryContext";
 
 export default function CategoryScreen(){
+    const {categories} = useCategories();
     return(
-        <ScrollView style={{flex: 1}}>
+        <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
             <Header />
+            {/* <CategoryCard/> */}
+            <View style={{marginHorizontal: 13, marginVertical: 15}}>
+                <FlatList
+                    data={categories}
+                    scrollEnabled={false}
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <CategoryCard />
+                    )}
+                    ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+                />
+            </View>
         </ScrollView>
     );
 }
